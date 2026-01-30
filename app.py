6fab6e9ee1dc83de5dd3f43f31f25815f26def13
Sivaananth03 <154042877+Sivaananth03@users.noreply.github.com>
2026-01-30 14:49:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+
+def home():
+
+    return "<h1>Docker Flask App Running Successfully</h1><h2>S V Sivaananth</h2>"
+
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=5000)
